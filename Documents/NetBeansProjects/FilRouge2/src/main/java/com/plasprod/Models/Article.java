e6ca0ecef6193ef9519594a57fe98dcdc09c5ad6
AntoineCesi <mist3r_whit3@hotmail.com>
2014-03-24 16:30:10 +0100

    Ajout Article

--- Documents/NetBeansProjects/FilRouge2/src/main/java/com/plasprod/Models/Article.java
+++ Documents/NetBeansProjects/FilRouge2/src/main/java/com/plasprod/Models/Article.java
@@ -12,4 +12,92 @@ package com.plasprod.Models;
  */
 public class Article {
     private long id;
+    private String reference;
+    private String nom;
+    private String couleur;
+    private Double quantiteStock;
+    private String unite;
+    private Double prixUnitaire;
+    private Boolean actif;
+
+    public Article(long id, String reference, String nom, String couleur, Double quantiteStock, String unite, Double prixUnitaire, Boolean actif) {
+        this.id = id;
+        this.reference = reference;
+        this.nom = nom;
+        this.couleur = couleur;
+        this.quantiteStock = quantiteStock;
+        this.unite = unite;
+        this.prixUnitaire = prixUnitaire;
+        this.actif = actif;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(String couleur) {
+        this.couleur = couleur;
+    }
+
+    public Double getQuantiteStock() {
+        return quantiteStock;
+    }
+
+    public void setQuantiteStock(Double quantiteStock) {
+        this.quantiteStock = quantiteStock;
+    }
+
+    public String getUnite() {
+        return unite;
+    }
+
+    public void setUnite(String unite) {
+        this.unite = unite;
+    }
+
+    public Double getPrixUnitaire() {
+        return prixUnitaire;
+    }
+
+    public void setPrixUnitaire(Double prixUnitaire) {
+        this.prixUnitaire = prixUnitaire;
+    }
+
+    public Boolean isActif() {
+        return actif;
+    }
+
+    public void setActif(Boolean actif) {
+        this.actif = actif;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" + "id=" + id + ", reference=" + reference + ", nom=" + nom + ", couleur=" + couleur + ", quantiteStock=" + quantiteStock + ", unite=" + unite + ", prixUnitaire=" + prixUnitaire + ", actif=" + actif + '}';
+    }
+    
 }
